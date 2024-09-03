60e41343d6811c3732b6c937a78180db244614b7
Linuxgeekforgeeks <mahmoudalisaaa@gmail.com>
2024-09-03 05:28:12 -0700

    completing the basic Animation

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,31 @@ import Tab1 from './pages/Tab1'
 import Tab2 from './pages/Tab2'
 import Tab3 from './pages/Tab3'
 import Tab4 from './pages/Tab4'
+import styled from 'styled-components'
 
 function App() {
   const [activeTab, setActiveTab] = useState(1)
   return (
     <>
     <Navbar activeTab={activeTab} setActiveTab={setActiveTab}/>
+    <AppStyled>
+
     {activeTab===1 && <Tab1/>}
     {activeTab===2 && <Tab2/>}
     {activeTab===3 && <Tab3/>}
     {activeTab===4 && <Tab4/>}
+    </AppStyled>
     </>
   )
 }
 
 export default App
+
+
+const AppStyled=styled.main`
+display: flex;
+height: 100vh;
+align-items: center;
+justify-content: center;
+  
+`
--- src/pages/Tab1.jsx
+++ src/pages/Tab1.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import { motion} from 'framer-motion'
 
 function Tab1() {
-  return (
-    <TabStyed>Tab1</TabStyed>
+    return (
+        <TabStyled>
+      <motion.div
+        initial={{ scale: 0.5, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        This is the basic Animation
+      </motion.div>
+    </TabStyled>
   )
 }
 
 export default Tab1
 
 
-const TabStyed=styled.div`
+const TabStyled = styled.div`
     
 `
