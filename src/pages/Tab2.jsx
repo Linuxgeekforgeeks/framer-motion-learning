1f9a27f337fef23e5f5c0f84fd68b93ab10a8fd7
Linuxgeekforgeeks <mahmoudalisaaa@gmail.com>
2024-09-03 05:34:55 -0700

    completed Hover Effect Animation

--- src/pages/Tab2.jsx
+++ src/pages/Tab2.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import { motion } from 'framer-motion'
+import styled from 'styled-components'
 function Tab2() {
   return (
-    <div>Tab2</div>
+
+    <Tabstyled>
+
+    <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ duration: 0.2 }}
+            className="animated-button"
+          >
+            Hover Me
+          </motion.button>
+    </Tabstyled>
   )
 }
 
 export default Tab2
+
+
+const Tabstyled=styled.div`
+    .animated-button{
+        padding: .4rem 2rem;
+        border-radius:0.3rem;
+        border: 1px solid gray;
+        cursor: pointer;
+    }
+`
